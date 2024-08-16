40b100962711afd0e93a7f46c6f1383bae2af000
Paul Kuelpmann <paulkuelpman@gmail.com>
2024-08-16 17:10:05 +0200

    Bitte bitte bitte
    4,5 Stunden, bitte lass es endlich funktionieren, sonst muss dieser Pi gleich sterben

--- src/script/capture_image.sh
+++ src/script/capture_image.sh
@@ -4,7 +4,7 @@
 output_file="assets/image.jpg"
 
 # Capture image with rpicam-still, suppressing output
-rpicam-still -n --output "$output_file"  >/dev/null 2>&1
+libcamera-still -o -n --output "$output_file"  >/dev/null 2>&1
 
 # Check if the image was captured successfully
 if [ $? -ne 0 ]; then
