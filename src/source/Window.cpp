d48e0858d8eb42e2710e537375efcde3e18ec115
Paul Kuelpmann <paulkuelpman@gmail.com>
2024-08-17 02:25:25 +0200

    Update Window.cpp

--- src/source/Window.cpp
+++ src/source/Window.cpp
@@ -5,7 +5,7 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent),
       imageLabel(new QLabel(this)),
-      percentageLabel(new QLabel("Futter: 0%", this)),
+      percentageLabel(new QLabel("Futter: ...", this)),
       temperatureLabel(new QLabel("Temperatur: 0°C", this)),
       humidityLabel(new QLabel("Feuchtigkeits: 0%", this)) {
     
