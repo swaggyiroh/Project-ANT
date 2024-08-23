6edc4e3f3258e5b8f34a572ee0f7b187bd3894c5
Paul Kuelpmann <paulkuelpman@gmail.com>
2024-08-23 14:52:45 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,6 +7,9 @@ is_package_installed() {
     dpkg -s "$1" &> /dev/null
 }
 
+# General update and upgrade
+sudo apt-get update && sudo apt-get upgrade -y
+
 # Check if WiringPi is installed
 if ! is_package_installed wiringpi; then
     echo "WiringPi is not installed. Installing..."
@@ -33,6 +36,20 @@ if ! is_package_installed libopencv-dev; then
     echo "OpenCV installed successfully."
 else
     echo "OpenCV is already installed."
+fi
+
+# Check if Qt is installed
+if ! is_package_installed qtbase5-dev; then
+    echo "Qt is not installed. Installing..."
+    sudo apt-get install -y build-essential libgl1-mesa-dev
+    sudo apt-get install -y qtbase5-dev qt5-qmake qtbase5-dev-tools
+    if [ $? -ne 0 ]; then
+        echo "Failed to install Qt. Exiting."
+        exit 1
+    fi
+    echo "Qt installed successfully."
+else
+    echo "Qt is already installed."
 fi
 
 echo "Installation check completed."
