b28878aeb9ddc4b10e82a7a2b47278e5de0db7ec
Paul Kuelpmann <paulkuelpman@gmail.com>
2024-08-23 16:53:44 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 
-
-rm -rf .git
 # Function to check if a package is installed
 is_package_installed() {
     dpkg -s "$1" &> /dev/null
 }
 
 # General update and upgrade
+cd
 sudo apt-get update && sudo apt-get upgrade -y
+
+# Check if Git is installed
+if ! is_package_installed git; then
+    echo "Git is not installed. Installing..."
+    sudo apt-get install -y git
+    if [ $? -ne 0 ]; then
+        echo "Failed to install Git. Exiting."
+        exit 1
+    fi
+    echo "Git installed successfully."
+else
+    echo "Git is already installed."
+fi
 
 # Check if WiringPi is installed
 if ! is_package_installed wiringpi; then
     echo "WiringPi is not installed. Installing..."
-    sudo apt-get update
-    sudo apt-get install -y wiringpi
+    git clone https://github.com/WiringPi/WiringPi.git
+    cd WiringPi
+    ./build
+    cd
     if [ $? -ne 0 ]; then
         echo "Failed to install WiringPi. Exiting."
         exit 1
@@ -54,7 +68,7 @@ fi
 
 echo "Installation check completed."
 
-
+cd Project-ANT*
 mkdir build
 qmake ..
 make
