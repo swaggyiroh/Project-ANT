709ac4df6c8b9c767a8c139c0d7049a946aace2f
Paul Kuelpmann <paulkuelpman@gmail.com>
2024-08-23 21:11:29 +0200

    Update ImageProcessing.cpp

--- src/source/ImageProcessing.cpp
+++ src/source/ImageProcessing.cpp
@@ -20,26 +20,43 @@ std::string ImageProcessing::processCapturedImage(const std::string& filename) {
         return "";
     }
 
-    Scalar red_lower(0, 0, 100); //Scalar is BGR = BLUE ; RED ; GREEN
-    Scalar red_upper(70, 60, 255);
+    Scalar red_lower1(0, 0, 100); //Scalar is BGR = BLUE ; RED ; GREEN
+    Scalar red_upper1(90, 90, 255);
 
-    Scalar green_lower(0, 50, 0);
-    Scalar green_upper(100, 255, 200);
+    Scalar green_lower1(0, 100, 0);
+    Scalar green_upper1(90, 255, 90);
 
-    Scalar blue_lower(50, 0, 0);
-    Scalar blue_upper(255, 150, 50);
+    Scalar green_lower2(0, 180, 0);
+    Scalar green_upper2(160, 255, 120);
+
+    Scalar green_lower3(0, 40, 0);
+    Scalar green_upper3(20, 100, 20);
     
-    // Create masks for each color in BGR
-    Mat red_mask, green_mask, blue_mask;
-    inRange(image, red_lower, red_upper, red_mask);
-    inRange(image, green_lower, green_upper, green_mask);
-    inRange(image, blue_lower, blue_upper, blue_mask);
+    Scalar blue_lower1(100, 0, 0);
+    Scalar blue_upper1(255, 90, 90);
 
+    Scalar blue_lower2(180, 100, 0);
+    Scalar blue_upper2(255, 160, 70);
+    
+    // Create masks for each color in RGB
+    Mat red_mask, green_mask1, green_mask2, green_mask3, blue_mask1, blue_mask2;
+    inRange(image, red_lower1, red_upper1, red_mask);
+    inRange(image, green_lower1, green_upper1, green_mask1);
+    inRange(image, green_lower2, green_upper2, green_mask2);
+    inRange(image, green_lower3, green_upper3, green_mask3);
+    inRange(image, blue_lower1, blue_upper1, blue_mask1);
+    inRange(image, blue_lower2, blue_upper2, blue_mask2);
+
+    // Combine masks if there are multiple ranges
+    Mat green_mask = green_mask1 | green_mask2 | green_mask3;
+    Mat blue_mask = blue_mask1 | blue_mask2;
+    
     // Count the number of non-zero pixels in each mask
     int totalPixels = image.rows * image.cols;
     int redPixels = countNonZero(red_mask);
     int greenPixels = countNonZero(green_mask);
     int bluePixels = countNonZero(blue_mask);
+
 
     //param to tune
     //int totalPixels = totalPixels / 10;
